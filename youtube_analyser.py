71797a7636b5586487e6c0b198f9f2859f2ea076
Yash Gupta <Yash Gupta>
2026-08-19 12:02:31 +0530

    LLM CHANGE

--- youtube_analyser.py
+++ youtube_analyser.py
@@ -8,7 +8,7 @@ load_dotenv()
 def youtube_agent():
     return Agent(
         name="YouTube Agent",
-        model=OpenAIChat(id="openrouter/auto" , base_url="https://openrouter.ai/api/v1" , api_key=os.getenv("OPENROUTER_API_KEY")),
+        model=OpenAIChat(id="openrouter/free" , base_url="https://openrouter.ai/api/v1" , api_key=os.getenv("OPENROUTER_API_KEY")),
         tools=[YouTubeTools()],
         instructions=dedent("""\
             You are an expert YouTube content analyst with a keen eye for detail! 🎓
